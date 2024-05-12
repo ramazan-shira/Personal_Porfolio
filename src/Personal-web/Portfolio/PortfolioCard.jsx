89e5e8cab64cf632d0fabab01718d207597d810f
Ramazan Shira <ramazanshira1@gmail.com>
2024-05-12 23:34:25 +0200

    visual changes

--- src/Personal-web/Portfolio/PortfolioCard.jsx
+++ src/Personal-web/Portfolio/PortfolioCard.jsx
@@ -1,11 +1,14 @@
 import "./portfolio.css";
-import personalPortfolio from "../../Assets/Images/personal-portfolio.png";
-import toDoList from "../../Assets/Images/todo-list.png";
-import tasteIt from "../../Assets/Images/taste-it.png";
-import addMembers from "../../Assets/Images/add-members.png";
-import llotoB from "../../Assets/Images/llotob.png";
-import guessNr from "../../Assets/Images/guess-nr.png";
-import rollDice from "../../Assets/Images/roll-dice.png";
+import personalPortfolio from "../../Assets/Images/personalPortfolio.png";
+import notes from "../../Assets/Images/notes-react.png";
+import todolistReact from "../../Assets/Images/todolist=react.png";
+import toDoList from "../../Assets/Images/toDoList.png";
+import digitalmastery from "../../Assets/Images/digitalmastery.png";
+import llotoB from "../../Assets/Images/llotoB.png";
+import guessNr from "../../Assets/Images/guessNr.png";
+import tasteIt from "../../Assets/Images/tasteIt.png";
+import rollDice from "../../Assets/Images/rollDice.png";
+
 import "./portfolioDark.css";
 
 const PortfolioCard = (props) => {
@@ -14,60 +17,69 @@ const PortfolioCard = (props) => {
   return (
     <div className="portfolio-cards-container">
       <div className="portfolio-cards-container-column1">
-        <div className={darkMode ? "card bg-1 dark" : "card bg-1"}>
-          <a href="https://ramazanshira.netlify.app/">
+        <div className={darkMode ? "card bg-1  dark" : "card bg-1 "}>
+          <a href="https://rsh-portfolio.netlify.app/">
             <img src={personalPortfolio} alt="Personal Portfolio website" />
           </a>
           <p className="subtitle">Personal Portfolio Website</p>
-          <a href="https://ramazanshira.netlify.app/">
+          <a href="https://rsh-portfolio.netlify.app/">
             <p className="title">My Portfolio</p>
           </a>
         </div>
 
         <div className={darkMode ? "card bg-2 dark" : "card bg-2"}>
-          <a href="https://tasteit-restaurant.netlify.app/">
-            <img src={tasteIt} alt="Taste It" />
+          <a href="https://rsh-notesapp-react.netlify.app/">
+            <img src={notes} alt="notes" />
           </a>
-          <p className="subtitle">Reastaurant Website</p>
-          <a href="https://tasteit-restaurant.netlify.app/">
-            <p className="title">Taste It</p>
+          <p className="subtitle">Notes application</p>
+          <a href="https://rsh-notesapp-react.netlify.app/">
+            <p className="title">Notes - React App</p>
           </a>
         </div>
 
-        <div className={darkMode ? "card bg-1 dark" : "card bg-1"}>
-          <a href="https://addusers-app.netlify.app/">
-            <img src={addMembers} alt="Add Members" />
+        <div className={darkMode ? "card bg-1 long dark" : "card bg-1 long"}>
+          <a href="https://zani-todolist.netlify.app/">
+            <img src={todolistReact} alt="to do list" />
           </a>
-          <p className="subtitle">Add New Members </p>
-          <a href="https://addusers-app.netlify.app/">
-            <p className="title">Add Members</p>
+          <p className="subtitle">To Do List</p>
+          <a href="https://zani-todolist.netlify.app/">
+            <p className="title">To Do List - React App</p>
           </a>
         </div>
 
         <div className={darkMode ? "card bg-2 dark" : "card bg-2"}>
-          <a href="https://rsh-roll-dice.netlify.app/">
-            <img src={rollDice} alt="Roll Dice" />
+          <a href="https://digitalmastery.netlify.app/">
+            <img src={digitalmastery} alt="digitalmastery" />
           </a>
-          <p className="subtitle">Roll Gice Game</p>
+          <p className="subtitle">Course/Training center website</p>
+          <a href="https://digitalmastery.netlify.app/">
+            <p className="title">DigitalMastery</p>
+          </a>
+        </div>
+
+        <div className={darkMode ? "card bg-2 long dark" : "card bg-2"}>
+          <a href="https://rsh-roll-dice.netlify.app/">
+            <img src={rollDice} alt="roll dice" />
+          </a>
+          <p className="subtitle">Roll Dice Game</p>
           <a href="https://rsh-roll-dice.netlify.app/">
             <p className="title">Roll Dice</p>
           </a>
         </div>
       </div>
-
       <div className="portfolio-cards-container-column2">
-        <div className={darkMode ? "card bg-2 long dark" : "card bg-2"}>
+        <div className={darkMode ? "card bg-2  dark" : "card bg-2"}>
           <a href="https://rsh-todolist.netlify.app/">
             <img src={toDoList} alt="To Do List" />
           </a>
-          <p className="subtitle">To Do Tasks</p>
+          <p className="subtitle">To Do List application</p>
           <a href="https://rsh-todolist.netlify.app/">
             {" "}
-            <p className="title">To Do List App</p>
+            <p className="title">To Do List - JavaScript App</p>
           </a>
         </div>
 
-        <div className={darkMode ? "card bg-1 dark" : "card bg-1"}>
+        <div className={darkMode ? "card bg-1 long dark" : "card bg-1 long"}>
           <a href="https://llotob-app.netlify.app/">
             <img src={llotoB} alt="Lloto B" />
           </a>
@@ -84,6 +96,16 @@ const PortfolioCard = (props) => {
           <p className="subtitle">Guess Number Game</p>
           <a href="https://rsh-guess-number.netlify.app/">
             <p className="title">Guess Number</p>
+          </a>
+        </div>
+
+        <div className={darkMode ? "card bg-1 long dark" : "card bg-1 long"}>
+          <a href="https://tasteit-restaurant.netlify.app/">
+            <img src={tasteIt} alt="taste it" />
+          </a>
+          <p className="subtitle">Taste IT - Restaurant website</p>
+          <a href="https://tasteit-restaurant.netlify.app/">
+            <p className="title">Taste IT</p>
           </a>
         </div>
       </div>
