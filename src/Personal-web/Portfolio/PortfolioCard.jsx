e3b25eab2534b77a4c3f3acfb0d3519e7424a964
Ramazan Shira <ramazanshira1@gmail.com>
2024-09-10 13:40:13 +0200

    CV Updated

--- src/Personal-web/Portfolio/PortfolioCard.jsx
+++ src/Personal-web/Portfolio/PortfolioCard.jsx
@@ -6,7 +6,7 @@ const PortfolioCard = (props) => {
   const { darkMode, title, subtitle, url, img } = props;
   return (
     <div className={darkMode ? "card  dark" : "card"}>
-      <a href={url}>
+      <a href={url} target="_blank">
         <img src={img} alt={title} />
       </a>
       <p className="subtitle">{subtitle}</p>
